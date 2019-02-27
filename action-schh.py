e751491bb67859f7f8b38b6b7e5280e06bc8b38d
Thomas Francis Jr <franc@tcsaf.com>
2019-02-27 14:45:00 -0500

    Initial version
    Uses pyharmony to control a Harmony Hub.  It'd be nicer to use
    aioharmony, but asyncio and snips don't seem to play nicely.

---
+++ action-schh.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+"""Snips skill action for Harmony Hub"""
+import configparser
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+from schh.schh import SmartCommandsHarmonyHub
+
+CONFIG_INI = "config.ini"
+CONFIG_ENCODING = "utf-8"
+
+class  SnipsConfigParser(configparser.ConfigParser):
+    """Subclass ConfigParser.SafeConfigParser to add to_dict method."""
+    def to_dict(self):
+        """Returns a dictionary of sections and options from the config file"""
+        return {section: {option_name : option for option_name,
+                          option in self.items(section)} for section in self.sections()}
+
+def read_configuration_file(file_name):
+    """Reads and parses file_name.  Returns a dictionary based on its contents"""
+    try:
+        config_parser = SnipsConfigParser()
+        config_parser.read(file_name)
+        return config_parser.to_dict()
+    except (IOError, configparser.Error):
+        return dict()
+
+def change_volume(hermes, intent_message):
+    """Handles intent for changing the volume"""
+    which_command = None
+    repeat = 1
+    for (slot_value, slot) in intent_message.slots.items():
+        if slot_value == "updownmute":
+            which_command = slot[0].slot_value.value.value
+        elif slot_value == "repeat":
+            repeat = int(float(slot[0].slot_value.value.value))
+
+    if which_command is None:
+        hermes.publish_end_session(intent_message.session_id,
+            "I did not change the volume with the Harmony Hub.")
+        return
+
+    hermes.skill.send_command(which_command, repeat)
+
+def send_command(hermes, intent_message):
+    """Handles intent for sending a command"""
+    which_command = None
+    repeat = 1
+    for (slot_value, slot) in intent_message.slots.items():
+        if slot_value == "command":
+            which_command = slot[0].slot_value.value.value
+        elif slot_value == "repeat":
+            repeat = int(float(slot[0].slot_value.value.value))
+
+    if which_command is None:
+        hermes.publish_end_session(intent_message.session_id,
+            "I did not send a command to the Harmony Hub.")
+        return
+
+    hermes.skill.send_command(which_command, repeat)
+
+def power_on(hermes, intent_message):
+    """Handles intent for power on (starting an activity)"""
+    activity = None
+    for (slot_value, slot) in intent_message.slots.items():
+        if slot_value == "activity":
+            activity = slot[0].slot_value.value.value
+
+    if activity is None:
+        hermes.publish_end_session(intent_message.session_id,
+            "I did not start an activity on the Harmony Hub.")
+        return
+
+    ret = hermes.skill.start_activity(activity)
+    if ret == 1:
+        sentence = "I started the {} activity on the Harmony Hub.".format(activity)
+    elif ret == -1:
+        sentence = "The {} activity is already started on the Harmony Hub.".format(activity)
+    else:
+        sentence = "I failed to started the {} activity on the Harmony Hub.".format(activity)
+    hermes.publish_end_session(intent_message.session_id, sentence)
+
+def main(hermes):
+    """main function"""
+    config = read_configuration_file(CONFIG_INI)
+    hermes.skill = SmartCommandsHarmonyHub(config["secret"]["remotename"])
+
+    hermes.skill.inject_activities()
+
+    hermes.subscribe_intent("franc:harmony_hub_volume", change_volume) \
+          .subscribe_intent("franc:harmony_hub_send_command", send_command) \
+          .subscribe_intent("franc:harmony_hub_power_on", power_on) \
+          .loop_forever()
+
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+        main(h)
---
+++ schh/schh.py
@@ -0,0 +1,137 @@
+"""Provides SmartCommandsHarmonyHub for smarter interaction with a HarmonyHub"""
+import json
+from subprocess import Popen, PIPE, STDOUT
+from pyharmony import client as harmony_client
+
+class SmartCommandsHarmonyHub:
+    """Class for interacting with a Harmony Hub in a smarter way"""
+    def __init__(self, remote_address):
+        """Initialize members
+
+        remote_address: The host name or IP address of the Harmony Hub
+        """
+        self.remote_address = remote_address
+        self.config = None
+        self.main_device = -1
+        self.volume_device = -1
+        self.activity_id = -1
+        self.activity_name = "Power Off"
+        self._connect()
+
+    def _reset_state_info(self):
+        """Resets state-related members to their defaults"""
+        self.config = None
+        self.main_device = -1
+        self.volume_device = -1
+        self.activity_id = -1
+        self.activity_name = "Power Off"
+
+    def _close(self):
+        """Closes the connectoion to the Harmony Hub"""
+        self.harmony.disconnect()
+        self._reset_state_info()
+
+    def _connect(self):
+        """Connects to the Harmony Hub"""
+        self.harmony = harmony_client.create_and_connect_client(self.remote_address, 5222)
+        self.config = self.harmony.get_config()
+        self.activity_id = self.harmony.get_current_activity()
+        activity = [x for x in self.config["activity"] if int(x["id"]) == self.activity_id][0]
+        if type(activity) is dict:
+            self.activity_name = activity["label"]
+
+    def _get_devices_for_activity(self):
+        """Gets the main and volume devices for the current activity"""
+        self.main_device = -1
+        self.volume_device = -1
+        if self.activity_id == -1:
+            return
+
+        act = next((x for x in self.config["activity"]
+                    if x["label"] == self.activity_name), None)
+        if act is not None:
+            #print(json.dumps(act, indent=4, separators=(",", ": ")))
+            if "ChannelChangingActivityRole" in act.keys():
+                self.main_device = act["ChannelChangingActivityRole"]
+            elif "roles" in act.keys() and "ChannelChangingActivityRole" in act["roles"].keys():
+                self.main_device = act["roles"]["ChannelChangingActivityRole"]
+            elif "roles" in act.keys() and "PlayMediaActivityRole" in act["roles"].keys():
+                self.main_device = act["roles"]["PlayMediaActivityRole"]
+            elif "roles" in act.keys() and "PlayMovieActivityRole" in act["roles"].keys():
+                self.main_device = act["roles"]["PlayMovieActivityRole"]
+
+            if "VolumeActivityRole" in act.keys():
+                self.volume_device = act["VolumeActivityRole"]
+            elif "roles" in act.keys() and "VolumeActivityRole" in act["roles"].keys():
+                self.volume_device = act["roles"]["VolumeActivityRole"]
+
+    def _get_device_for_command(self, command):
+        self._get_devices_for_activity()
+        if (command == "VolumeUp") or (command == "VolumeDown") or (command == "Mute"):
+            return self.volume_device
+        return self.main_device
+
+    def _get_activities_payload(self, activities):
+        return ["addFromVanilla", {"harmony_hub_activities_name": activities}]
+
+    def _get_update_payload(self):
+        operations = []
+        activities = []
+        for activity in self.config["activity"]:
+            activities.append(activity["label"])
+
+        operations.append(self._get_activities_payload(activities))
+        payload = {"operations": operations}
+        return json.dumps(payload)
+
+    def send_command(self, command, repeat):
+        """Sends command to the Harmony Hub repeat times"""
+        self._connect()
+        send_commands = []
+        device = self._get_device_for_command(command)
+        for _ in range(repeat):
+            self.harmony.send_command(device, command, 0.1)
+        self._close()
+        return
+
+    def current_activity(self):
+        """Returns the ID and name of the current activity"""
+        self._connect()
+        return_values = (self.activity_id, self.activity_name)
+        self._close()
+        return return_values
+
+    def start_activity(self, activity_name):
+        """Starts an activity on the Harmony Hub"""
+        self._connect()
+        if activity_name == self.activity_name:
+            print("current activity is the same as what was requested, doing nothing")
+            return -1
+
+        activity = [x for x in self.config["activity"] if x["label"] == activity_name][0]
+        if type(activity) is dict:
+            activity_id = activity["id"]
+        return_value = self.harmony.start_activity(activity_id)
+        self._close()
+        return 1 if return_value else 0
+
+    def power_off(self):
+        """Sets the current activity of the Harmony Hub to -1, AKA "PowerOff"."""
+        return self.start_activity("PowerOff")
+
+    def inject_activities(self):
+        """Injects the list of activities known to the Harmony Hub"""
+        self._connect()
+        payload = self._get_update_payload() + "\n"
+        pipe = Popen(["/usr/bin/mosquitto_pub",
+                      "-t",
+                      "hermes/injection/perform",
+                      "-l"
+                     ],
+                     stdin=PIPE,
+                     stdout=PIPE,
+                     stderr=STDOUT)
+        pipe.communicate(input=payload.encode("utf-8"))
+        self._close()
+
+__all__ = ["SmartCommandsHarmonyHub"]
