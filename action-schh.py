aba748f4d04804cf9b71a30105e0b5e0e5a898dc
Thomas Francis Jr <franc@tcsaf.com>
2019-03-01 13:56:59 -0500

    Fixed slot name for change_channel

--- action-schh.py
+++ action-schh.py
@@ -29,7 +29,7 @@ def change_channel(hermes, intent_message):
     channel_slot = None
     repeat = 1
     for (slot_value, slot) in intent_message.slots.items():
-        if slot_value == "channel":
+        if slot_value == "channel_number":
             channel_slot = slot[0].slot_value.value.value
 
     if channel_slot is None:
