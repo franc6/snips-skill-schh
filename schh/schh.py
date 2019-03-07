franc6/snips-skill-schh

commit 825bf0aee6e89322d405272f758d14ce56017f59

Changed test when checking activity names
We shouldn't get the intent for an unknown activity anyway, but it
happens, so be sure to check it correctly.

schh/schh.py
@@ -173,7 +173,7 @@ class SmartCommandsHarmonyHub:
             return -2
 
         activities = [x for x in self.config["activity"] if x["label"] == activity_name]
-        if activities is None:
+        if activities:
             print("Cannot find the activity: {} ".format(activity_name))
             return -3
         activity = activities[0]

commit 15a8d4da60a4a05ddc60f3873f4ad85b030bc8f4

Fixed opposite test for finding activity.
See commit 825bf0aee6e89322d405272f758d14ce56017f59 which broke this.

schh/schh.py
@@ -173,7 +173,7 @@ class SmartCommandsHarmonyHub:
             return -2
 
         activities = [x for x in self.config["activity"] if x["label"] == activity_name]
-        if activities:
+        if not activities:
             print("Cannot find the activity: {} ".format(activity_name))
             return -3
         activity = activities[0]