3871ff33c1a7971fa8897a86870e283ac2a70867
Thomas Francis Jr <franc@tcsaf.com>
2019-06-10 14:17:36 -0400

    Updated code so it's based on snipskit

--- action-schh.py
+++ action-schh.py
@@ -1,177 +1,155 @@
 #!/usr/bin/env python3
 """Snips skill action for Harmony Hub"""
-import configparser
 import gettext
 import locale
-from hermes_python.hermes import Hermes
-from hermes_python.ontology import *
+
+from snipskit.hermes.apps import HermesSnipsApp
+from snipskit.config import AppConfig
+from snipskit.hermes.decorators import intent
 
 locale.setlocale(locale.LC_ALL, '')
 gettext.bindtextdomain('messages', 'locales')
 gettext = gettext.gettext
 
-CONFIG_INI = "config.ini"
-CONFIG_ENCODING = "utf-8"
+class SCHHActions(HermesSnipsApp):
+    skill = False
 
-class  SnipsConfigParser(configparser.ConfigParser):
-    """Subclass ConfigParser.SafeConfigParser to add to_dict method."""
-    def to_dict(self):
-        """Returns a dictionary of sections and options from the config file"""
-        return {section: {option_name : option for option_name,
-                          option in self.items(section)} for section in self.sections()}
+    def _send_command(hermes, intent_message, which_command, repeat, delay=0.1):
+        print("self._send_command: ", which_command, repeat, delay)
+        ret = self.skill.send_command(which_command, repeat, delay)
+        if ret == -1:
+            hermes.publish_end_session(intent_message.session_id,
+                gettext("FAILED_CONNECT"))
+        elif ret == 0:
+            hermes.publish_end_session(intent_message.session_id,
+                gettext("COMMAND_NOT_FOUND"))
 
-def read_configuration_file(file_name):
-    """Reads and parses file_name.  Returns a dictionary based on its contents"""
-    try:
-        config_parser = SnipsConfigParser()
-        config_parser.read(file_name)
-        return config_parser.to_dict()
-    except (IOError, configparser.Error):
-        return dict()
+    @intent('franc:harmony_hub_change_channel')
+    def change_channel(self, hermes, intent_message):
+        """Handles intent for changing the channel"""
+        channel_slot = None
+        repeat = 1
+        if intent_message.slots is not None:
+            if intent_message.slots.channel_number:
+                channel_slot = str(intent_message.slots.channel_number[0].slot_value.value.value)
 
-def _send_command(hermes, intent_message, which_command, repeat, delay=0.1):
-    print("_send_command: ", which_command, repeat, delay)
-    ret = hermes.skill.send_command(which_command, repeat, delay)
-    if ret == -1:
-        hermes.publish_end_session(intent_message.session_id,
-            gettext("FAILED_CONNECT"))
-    elif ret == 0:
-        hermes.publish_end_session(intent_message.session_id,
-            gettext("COMMAND_NOT_FOUND"))
+        if channel_slot is None:
+            hermes.publish_end_session(intent_message.session_id,
+                gettext("NO_CHANNEL_GIVEN"))
+            return
 
-def change_channel(hermes, intent_message):
-    """Handles intent for changing the channel"""
-    channel_slot = None
-    repeat = 1
-    if intent_message.slots is not None:
-        if intent_message.slots.channel_number:
-            channel_slot = str(intent_message.slots.channel_number[0].slot_value.value.value)
+        ret = self.skill.change_channel(channel_slot)
+        if ret == -1:
+            hermes.publish_end_session(intent_message.session_id,
+                gettext("FAILED_CONNECT"))
+        elif ret == 0:
+            hermes.publish_end_session(intent_message.session_id,
+                gettext("FAILED_CHANGE_CHANNEL"))
 
-    if channel_slot is None:
-        hermes.publish_end_session(intent_message.session_id,
-            gettext("NO_CHANNEL_GIVEN"))
-        return
+    @intent('franc:harmony_hub_volume')
+    def change_volume(self, hermes, intent_message):
+        """Handles intent for changing the volume"""
+        which_command = None
+        repeat = 1
+        if intent_message.slots is not None:
+            if intent_message.slots.updownmute:
+                which_command = intent_message.slots.updownmute[0].slot_value.value.value
+            if intent_message.slots.repeat:
+                repeat = int(float(intent_message.slots.repeat[0].slot_value.value.value))
 
-    ret = hermes.skill.change_channel(channel_slot)
-    if ret == -1:
-        hermes.publish_end_session(intent_message.session_id,
-            gettext("FAILED_CONNECT"))
-    elif ret == 0:
-        hermes.publish_end_session(intent_message.session_id,
-            gettext("FAILED_CHANGE_CHANNEL"))
+        if which_command is None:
+            hermes.publish_end_session(intent_message.session_id,
+                gettext("NO_VOLUME_GIVEN"))
+            return
 
-def change_volume(hermes, intent_message):
-    """Handles intent for changing the volume"""
-    which_command = None
-    repeat = 1
-    if intent_message.slots is not None:
-        if intent_message.slots.updownmute:
-            which_command = intent_message.slots.updownmute[0].slot_value.value.value
-        if intent_message.slots.repeat:
-            repeat = int(float(intent_message.slots.repeat[0].slot_value.value.value))
+        self._send_command(hermes, intent_message, which_command, repeat)
 
-    if which_command is None:
-        hermes.publish_end_session(intent_message.session_id,
-            gettext("NO_VOLUME_GIVEN"))
-        return
+    @intent('franc:harmony_hub_channel_surf')
+    def channel_surf(hermes, intent_message):
+        self._send_command(hermes, intent_message, "ChannelUp", 40, 8)
 
-    _send_command(hermes, intent_message, which_command, repeat)
+    @intent('franc:harmony_hub_send_command')
+    def send_command(self, hermes, intent_message):
+        """Handles intent for sending a command"""
+        which_command = None
+        repeat = 1
+        if intent_message.slots is not None:
+            if intent_message.slots.command:
+                which_command = intent_message.slots.command[0].slot_value.value.value
+            if intent_message.slots.repeat:
+                repeat = int(float(intent_message.slots.repeat[0].slot_value.value.value))
 
-def channel_surf(hermes, intent_message):
-    _send_command(hermes, intent_message, "ChannelUp", 40, 8)
+        if which_command is None:
+            hermes.publish_end_session(intent_message.session_id,
+                gettext("NO_COMMAND_GIVEN"))
+            return
 
-def send_command(hermes, intent_message):
-    """Handles intent for sending a command"""
-    which_command = None
-    repeat = 1
-    if intent_message.slots is not None:
-        if intent_message.slots.command:
-            which_command = intent_message.slots.command[0].slot_value.value.value
-        if intent_message.slots.repeat:
-            repeat = int(float(intent_message.slots.repeat[0].slot_value.value.value))
+        self._send_command(hermes, intent_message, which_command, repeat)
 
-    if which_command is None:
-        hermes.publish_end_session(intent_message.session_id,
-            gettext("NO_COMMAND_GIVEN"))
-        return
+    @intent('franc:harmony_hub_power_on')
+    def power_on(self, hermes, intent_message):
+        """Handles intent for power on (starting an activity)"""
+        activity = None
+        if intent_message.slots is not None:
+            if intent_message.slots.activity:
+                activity = intent_message.slots.activity[0].slot_value.value.value
 
-    _send_command(hermes, intent_message, which_command, repeat)
+        if activity is None:
+            hermes.publish_end_session(intent_message.session_id,
+                gettext("NO_ACTIVITY_GIVEN"))
+            return
 
-def power_on(hermes, intent_message):
-    """Handles intent for power on (starting an activity)"""
-    activity = None
-    if intent_message.slots is not None:
-        if intent_message.slots.activity:
-            activity = intent_message.slots.activity[0].slot_value.value.value
+        ret = self.skill.start_activity(activity)
+        if ret == 1:
+            sentence = gettext("STARTED_ACTIVITY").format(activity=activity)
+        elif ret == -1:
+            sentence = gettext("FAILED_CONNECT")
+        elif ret == -2:
+            sentence = gettext("ACTIVITY_ALREADY_STARTED").format(activity=activity)
+        elif ret == -3:
+            sentence = gettext("ACTIVITY_UNKNOWN").format(activity=activity)
+        else:
+            sentence = gettext("FAILED_START_ACTIVITY").format(activity=activity)
+        hermes.publish_end_session(intent_message.session_id, sentence)
 
-    if activity is None:
-        hermes.publish_end_session(intent_message.session_id,
-            gettext("NO_ACTIVITY_GIVEN"))
-        return
+    @intent('franc:harmony_hub_list_activities')
+    def list_activities(self, hermes, intent_message):
+        """Handles intent for listing activities"""
 
-    ret = hermes.skill.start_activity(activity)
-    if ret == 1:
-        sentence = gettext("STARTED_ACTIVITY").format(activity=activity)
-    elif ret == -1:
-        sentence = gettext("FAILED_CONNECT")
-    elif ret == -2:
-        sentence = gettext("ACTIVITY_ALREADY_STARTED").format(activity=activity)
-    elif ret == -3:
-        sentence = gettext("ACTIVITY_UNKNOWN").format(activity=activity)
-    else:
-        sentence = gettext("FAILED_START_ACTIVITY").format(activity=activity)
-    hermes.publish_end_session(intent_message.session_id, sentence)
+        activities = self.skill.list_activities()
+        if isinstance(activities, int) and activities == -1:
+            sentence = gettext("FAILED_CONNECT")
+        elif len(activities) == 0:
+            sentence = gettext("NO_ACTIVITIES_ON_HUB")
+        else:
+            act_sentence = ''
+            for activity in activities:
+                act_sentence += ',,.. ' + activity
+            sentence = gettext("ACTIVITIES_LIST").format(activities=act_sentence)
 
-def list_activities(hermes, intent_message):
-    """Handles intent for listing activities"""
+        print(sentence)
+        hermes.publish_end_session(intent_message.session_id, sentence)
 
-    activities = hermes.skill.list_activities()
-    if isinstance(activities, int) and activities == -1:
-        sentence = gettext("FAILED_CONNECT")
-    elif len(activities) == 0:
-        sentence = gettext("NO_ACTIVITIES_ON_HUB")
-    else:
-        act_sentence = ''
-        for activity in activities:
-            act_sentence += ',,.. ' + activity
-        sentence = gettext("ACTIVITIES_LIST").format(activities=act_sentence)
+    @intent('franc:harmony_hub_which_activity')
+    def which_activity(self, hermes, intent_message):
+        """Handles intent for listing which activity is current"""
 
-    print(sentence)
-    hermes.publish_end_session(intent_message.session_id, sentence)
+        ret_value = self.skill.current_activity()
+        if isinstance(ret_value, int) and ret_value == -1:
+            sentence = gettext("FAILED_CONNECT")
+        else:
+            (activity_id, activity_name) = ret_value
+            sentence = gettext("CURRENT_ACTIVITY").format(activity=activity_name)
+        hermes.publish_end_session(intent_message.session_id, sentence)
 
-def which_activity(hermes, intent_message):
-    """Handles intent for listing which activity is current"""
+    def initialize(self):
+        if self.config["secret"]["control"] == "XMPP":
+            from schh.schh import SmartCommandsHarmonyHub
+        else:
+            from schh.schhaio import SmartCommandsHarmonyHub
+        self.skill = SmartCommandsHarmonyHub(self.config["secret"]["remotename"])
 
-    ret_value = hermes.skill.current_activity()
-    if isinstance(ret_value, int) and ret_value == -1:
-        sentence = gettext("FAILED_CONNECT")
-    else:
-        (activity_id, activity_name) = ret_value
-        sentence = gettext("CURRENT_ACTIVITY").format(activity=activity_name)
-    hermes.publish_end_session(intent_message.session_id, sentence)
-
-def main(hermes):
-    """main function"""
-    config = read_configuration_file(CONFIG_INI)
-    if config["secret"]["control"] == "XMPP":
-        from schh.schh import SmartCommandsHarmonyHub
-    else:
-        from schh.schhaio import SmartCommandsHarmonyHub
-    hermes.skill = SmartCommandsHarmonyHub(config["secret"]["remotename"])
-
-    hermes.skill.inject_activities()
-
-    hermes.subscribe_intent("franc:harmony_hub_volume", change_volume) \
-          .subscribe_intent("franc:harmony_hub_send_command", send_command) \
-          .subscribe_intent("franc:harmony_hub_power_on", power_on) \
-          .subscribe_intent("franc:harmony_hub_change_channel", change_channel) \
-          .subscribe_intent("franc:harmony_hub_list_activities", list_activities) \
-          .subscribe_intent("franc:harmony_hub_which_activity", which_activity) \
-          .loop_forever()
-
-    print("loop_forever returned!")
-    hermes.skill.close()
+        self.skill.inject_activities()
 
 if __name__ == "__main__":
-    with Hermes("localhost:1883") as h:
-        main(h)
+    SCHHActions(config=AppConfig())
--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ fi
 
 . ${VENV}/bin/activate
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 if [ ! -e config.ini ]
 then
